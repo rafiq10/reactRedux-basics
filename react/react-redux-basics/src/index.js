717fadd69e267bbae744875f57a88f6668cf9b42
rafiq10 <rafiq10@gmail.com>
2018-03-17 20:08:27 +0100

    first reducer

--- react/react-redux-basics/src/index.js
+++ react/react-redux-basics/src/index.js
@@ -1,34 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
 
+// import Main from './components/Main';
+// import User from './components/User';
 
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             username: "Rafal"
+//         };
+//     }
 
-import Main from './components/Main';
-import User from './components/User';
+//     changeUsername(newName) {
+//         this.setState({
+//             username: newName
+//         });
+//     }
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "Rafal"
-        };
+//     render() {
+//         return (
+//             <div>
+//                 <Main changeUsername={this.changeUsername.bind(this)} />
+//                 <br />
+//                 <User username={this.state.username} />
+//             </div>
+//         );
+//     }
+// }
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import { createStore } from 'redux';
+
+const initialState = {
+    result: 1,
+    lastValues: [],
+    username: "Rafal"
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type){
+        case "ADD":
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
+        case "SUBTRACT":
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
+        default:
+            // state=0;
+            break;
     }
+    return state;
+};
+const store = createStore(reducer);
 
-    changeUsername(newName) {
-        this.setState({
-            username: newName
-        });
-    }
+store.subscribe(() => {
+    console.log("Store updated", store.getState());
+});
 
-    render() {
-        return (
-            <div>
-                <Main changeUsername={this.changeUsername.bind(this)} />
-                <br />
-                <User username={this.state.username} />
-            </div>
-        );
-    }
-}
-ReactDOM.render(<App />, document.getElementById('root'));
+store.dispatch({
+    type: "ADD",
+    payload: 100
+});
+
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 3
+});
