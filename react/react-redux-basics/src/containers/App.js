7cfd8d58ab9dae3fced9faf06f47c2842681dd4e
rafiq10 <rafiq10@gmail.com>
2018-03-18 12:28:08 +0100

    react containers

---
+++ react/react-redux-basics/src/containers/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import {Container} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import {Main} from '../components/Main';
+import {User} from '../components/User';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <Container>
+                <Main changeUsername={() => this.props.setName("Vero")} />
+                <br />
+                <User username={this.props.user.name} />
+            </Container>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return{
+        user: state.user,
+        math: state.math
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        setName: (name) => {
+            dispatch({
+                type: "SET_NAME",
+                payload: name
+            });
+        }
+    };
+};
+
+
+
+const kaka = connect(mapStateToProps,mapDispatchToProps)(App);
+export default kaka;
