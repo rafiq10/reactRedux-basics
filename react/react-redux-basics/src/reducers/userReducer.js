54e9408febe6a8dfceae21446231e19741f6ae17
rafiq10 <rafiq10@gmail.com>
2018-03-18 18:24:07 +0100

    promises in redux

--- react/react-redux-basics/src/reducers/userReducer.js
+++ react/react-redux-basics/src/reducers/userReducer.js
@@ -4,7 +4,7 @@ const userReducer = (state = {
     age: 27
 }, action) => {
     switch (action.type){
-        case "SET_NAME":
+        case "SET_NAME_FULFILLED":
             state = {
                 ...state,
                 name: action.payload
